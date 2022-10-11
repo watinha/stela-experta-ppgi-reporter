6976d9a89b910eebb56f3f61b72d7c93aac5c8ad
Willian Watanabe <watinha@gmail.com>
2022-10-11 11:50:36 -0300

    fix the order in which spreadsheats are presented

--- report.py
+++ report.py
@@ -54,28 +54,28 @@ prod_docente = prod_by_docente({ 'journal': journal, 'proc': proc, 'master': mas
 # general report
 with pd.ExcelWriter('./output/%s/program-report.xlsx' % (report_folder)) as writer:
     journal[['ABNT', 'Ano da produção', 'Periódico', 'qualis']].to_excel(writer, sheet_name='Journal')
+    prod_student['journal'][['ABNT', 'Ano da produção', 'Periódico', 'qualis']].to_excel(writer, sheet_name='Student-Journal')
     proc[['ABNT', 'Ano da produção', 'Periódico', 'Proceedings qualis']].to_excel(writer, sheet_name='Proceedings')
+    prod_student['proc'][['ABNT', 'Ano da produção', 'Periódico', 'Proceedings qualis']].to_excel(writer, sheet_name='Student-Proceedings')
+    tec[['ABNT', 'Tipo da produção', 'Ano da produção']].to_excel(writer, sheet_name='Tec')
+    prod_student['tec'][['ABNT', 'Tipo da produção', 'Ano da produção']].to_excel(writer, sheet_name='Student-Tec')
     master[['ABNT', 'Ano da produção']].to_excel(writer, sheet_name='Graduate')
     ic[['ABNT', 'Ano da produção']].to_excel(writer, sheet_name='IC')
     tcc[['ABNT', 'Ano da produção']].to_excel(writer, sheet_name='TCC')
-    tec[['ABNT', 'Tipo da produção', 'Ano da produção']].to_excel(writer, sheet_name='Tec')
-    prod_student['journal'][['ABNT', 'Ano da produção', 'Periódico', 'qualis']].to_excel(writer, sheet_name='Student-Journal')
-    prod_student['proc'][['ABNT', 'Ano da produção', 'Periódico', 'Proceedings qualis']].to_excel(writer, sheet_name='Student-Proceedings')
-    prod_student['tec'][['ABNT', 'Tipo da produção', 'Ano da produção']].to_excel(writer, sheet_name='Student-Tec')
 
 
 for professor in prod_docente.keys():
     name = ''.join(professor.split(' '))
     with pd.ExcelWriter('./output/%s/report-%s.xlsx' % (report_folder, name)) as writer:
         prod_docente[professor]['journal'][['ABNT', 'Ano da produção', 'Periódico', 'qualis']].to_excel(writer, sheet_name='Journal')
+        prod_docente[professor]['journal_student'][['ABNT', 'Ano da produção', 'Periódico', 'qualis']].to_excel(writer, sheet_name='Student-Journal')
         prod_docente[professor]['proc'][['ABNT', 'Ano da produção', 'Periódico', 'Proceedings qualis']].to_excel(writer, sheet_name='Proceedings')
+        prod_docente[professor]['proc_student'][['ABNT', 'Ano da produção', 'Periódico', 'Proceedings qualis']].to_excel(writer, sheet_name='Student-Proceedings')
+        prod_docente[professor]['tec'][['ABNT', 'Tipo da produção', 'Ano da produção']].to_excel(writer, sheet_name='Tec')
+        prod_docente[professor]['tec_student'][['ABNT', 'Tipo da produção', 'Ano da produção']].to_excel(writer, sheet_name='Student-Tec')
         prod_docente[professor]['master'][['ABNT', 'Ano da produção']].to_excel(writer, sheet_name='Graduate')
         prod_docente[professor]['ic'][['ABNT', 'Ano da produção']].to_excel(writer, sheet_name='IC')
         prod_docente[professor]['tcc'][['ABNT', 'Ano da produção']].to_excel(writer, sheet_name='TCC')
-        prod_docente[professor]['tec'][['ABNT', 'Tipo da produção', 'Ano da produção']].to_excel(writer, sheet_name='Tec')
-        prod_docente[professor]['journal_student'][['ABNT', 'Ano da produção', 'Periódico', 'qualis']].to_excel(writer, sheet_name='Student-Journal')
-        prod_docente[professor]['proc_student'][['ABNT', 'Ano da produção', 'Periódico', 'Proceedings qualis']].to_excel(writer, sheet_name='Student-Proceedings')
-        prod_docente[professor]['tec_student'][['ABNT', 'Tipo da produção', 'Ano da produção']].to_excel(writer, sheet_name='Student-Tec')
 
 
 with pd.ExcelWriter('./output/%s/professor-report.xlsx' % (report_folder)) as writer:
@@ -104,13 +104,14 @@ with pd.ExcelWriter('./output/%s/professor-report.xlsx' % (report_folder)) as wr
             tec_student_all = pd.concat([tec_student_all, prod_docente[professor]['tec_student'].loc[:, ['ABNT', 'Tipo da produção', 'Ano da produção', 'docente']]])
 
     journal_all.to_excel(writer, sheet_name='Journal')
+    journal_student_all.to_excel(writer, sheet_name='Journal Student')
     proc_all.to_excel(writer, sheet_name='Proceedings')
+    proc_student_all.to_excel(writer, sheet_name='Proceedings Student')
+    tec_all.to_excel(writer, sheet_name='Tec')
+    tec_student_all.to_excel(writer, sheet_name='Tec Student')
     master_all.to_excel(writer, sheet_name='Graduate')
     ic_all.to_excel(writer, sheet_name='IC')
     tcc_all.to_excel(writer, sheet_name='TCC')
-    journal_student_all.to_excel(writer, sheet_name='Journal Student')
-    proc_student_all.to_excel(writer, sheet_name='Proceedings Student')
-    tec_student_all.to_excel(writer, sheet_name='Tec Student')
 
 
 
